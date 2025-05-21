c26f205cb42e1918611638d597d23743f6da6043
Anmol-sudo <anmolshiv1974@gmail.com>
2025-05-21 14:25:37 +0530

    build the home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,21 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { View , StyleSheet} from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+
+const PlaceholderImage = require("@/assets/images/background-image.png")
 
 export default function Index() {
   return (
     <View
       style={styles.container}
     >
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>
-      Go to About Screen</Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -17,15 +24,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: 'center'
   },
-  text: {
-    color: '#fff'
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
   },
-  button: {
-    fontSize: 20, 
-    textDecorationLine: 'underline',
-    color: '#fff',
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
   }
 })
